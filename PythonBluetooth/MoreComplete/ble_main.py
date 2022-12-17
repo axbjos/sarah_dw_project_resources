c6d8eeb3eb3e6521fbb0783d1e62459399f8920e
Joe Axberg <jaxberg@umn.edu>
2022-12-17 14:32:18 -0600

    lots of update to main program

--- PythonBluetooth/MoreComplete/ble_main.py
+++ PythonBluetooth/MoreComplete/ble_main.py
@@ -1,13 +1,25 @@
+''' 
+Use "pip" (Pi) or "pip3" (Mac) to make sure dependencies are installed
+pip install bleak
+pip install paho-mqtt
+pip install asyncio 
+
+Reads data from a BLE Arduino running the code from the ArduinoBluetooth Directory
+Arduino is advertising one "characteristic" that is a temperature value 
+'''
 import asyncio
 from bleak import BleakClient
 import logging
-import sys
 import paho.mqtt.publish as publish
+#import sys #only needed if streaming logs to the console
 
-# use python loggin functionality
+#### program startup stuff ####
+# use the built-in python logging framework
+# code below will send logging data a file named ble_main.log in the 
+# directory as the code
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
-    level=logging.INFO,  #set logging level
+    level=logging.INFO,  #set logging level, set to DEBUG if you want more
     datefmt="%H:%M:%S",
     #stream=sys.stderr, #use this if you just want logs sent to console
     filename='ble_main.log',  #use this if you want logs sent to file.
@@ -15,6 +27,24 @@ logging.basicConfig(
 logger = logging.getLogger("areq")
 logging.getLogger("chardet.charsetprober").disabled = True
 
+### read config file
+config_file = open("ble_main.conf", "r")
+
+first_line = config_file.readline().split(":")
+ble_address = first_line[1][:-1]
+logger.info("BLE address is %s", ble_address)
+
+second_line = config_file.readline().split(":")
+characteristic_id = second_line[1][:-1]
+logger.info("Characterisic UUID is %s",characteristic_id)
+
+third_line = config_file.readline().split(":")
+mqtt_broker_ip = third_line[1][:-1]
+logger.info("MQTT Broker IP is %s",mqtt_broker_ip)
+
+config_file.close()
+
+### functions ###
 async def read_ble(address, characteristic_id):
 
     async with BleakClient(address) as client:
@@ -22,32 +52,29 @@ async def read_ble(address, characteristic_id):
         data = await client.read_gatt_char(characteristic_id)
         # data read from the bluetooth connection is going to return a byte array of hex
         # so take this data, decode as hex, then convert to an integer
-        arduino_raw_value = int(data.hex(),16)
-        arduino_fahrenheit = (arduino_raw_value*9.0/5.0)+32
-        logger.info("Raw Arduino Data: %s", arduino_raw_value)
-        logger.info("Converted Fahrenheit Temp: %s", arduino_fahrenheit)
+        arduino_raw_value = int(data.hex(),16)  #hex value
+        arduino_fahrenheit = (arduino_raw_value*9.0/5.0)+32   #fahrenheit integer
+        logger.debug("Raw Arduino Data: %s", arduino_raw_value)  #log hex value as a "debug" thing
+        logger.info("Converted Fahrenheit Temp: %s", arduino_fahrenheit)  #log fahrenheit as "info" thing
 
         return arduino_fahrenheit
 
-def publish_mqtt_data(temperature):
+def publish_mqtt_data(temperature, broker_ip):
 
-    publish.single("bluetooth", payload=temperature, hostname="192.168.68.70")
+    publish.single("bluetooth", payload=temperature, hostname=broker_ip)
+    logger.info("Temperature published to MQTT at 192.168.68.70")
 
 async def main():
-
-    # UUID of Arduino - Macs use UUID
-    address = "106C2542-49BA-1830-96E4-52DE905C54BF"
-    # UUID of the "characterisic" aka the value you want to read - the temperature
-    characteristic_id = "00002101-0000-1000-8000-00805f9b34fb"
     
     while True:
 
        #gather data from ble
-       temperature = await read_ble(address, characteristic_id)
+       temperature = await read_ble(ble_address, characteristic_id)
 
        #publish data to mqtt
-       publish_mqtt_data(temperature)
+       publish_mqtt_data(temperature, mqtt_broker_ip)
 
        await asyncio.sleep(5)
 
+### runner ###
 asyncio.run(main())
